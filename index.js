24a5c450d2b8d3b402ef9fe593f4892060b89b3c
nicolasmoise <nicolas.moise@gmail.com>
2017-04-13 21:29:43 -0400

    call API and parse reps

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const botBuilder = require('claudia-bot-builder');
+const rp = require('minimal-request-promise');
+const zipcodes = require('zipcodes');
+const _ = require('lodash');
 
-module.exports = botBuilder(function (message) {
-  return 'Hello World';
+
+function getRepresentatives(zip) {
+  const lookup = zipcodes.lookup(zip);
+
+  const options = {
+    method: 'GET',
+    hostname: 'phone-your-rep.herokuapp.com',
+    path: `/reps?&lat=${lookup.latitude}&long=${lookup.longitude}`
+  };
+
+  return rp(options).then(response => {
+
+    const fbTemplate = botBuilder.fbTemplate;
+
+    const reps = JSON.parse(response.body);
+
+    return _.map(reps, (rep) => {
+      return new fbTemplate.Button(rep.official_full)
+        .addCallButton('N.Y.C', '+9292823681')
+        .get();
+    });
+
+  }).catch(err => `API fail ${err}`);
+}
+
+module.exports = botBuilder((message, apiReq) => {
+  apiReq.lambdaContext.callbackWaitsForEmptyEventLoop = false;
+
+  try {
+    return getRepresentatives(message.text)
+  } catch(err) {
+    return err;
+  };
 });
